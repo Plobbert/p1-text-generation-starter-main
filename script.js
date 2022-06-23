7b59b270ac9c6ddf26d8fbd1029571d7c80bb7f0
Maxwell Mollison <81993009+Plobbert@users.noreply.github.com>
2022-06-23 15:00:53 -0700

    rethemed assignment

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 const fillers = {
-    adventurer: ["My dude", "Bro", "Adventurer", "Traveller", "Fellow", "Citizen", "Ashen One", "Dragonborn", "Cool person", "Tarnished", "$adventurer and $adventurer", "$adventurer, $adventurer, and $adventurer", "Geoff"],
-    pre: ["Fra", "Tro", "Gre", "Pan", "Ast", "Ara"],
-    post: ["gria", "ston", "gott","-on-the-lee", "ora", "Ara", "uwu"],
-    people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "orca"],
-    item: ["axe", "staff", "book", "cloak", "shield", "club", "sword", "magic gloves", "galvel", "fists", "mace", "potato"],
-    num: ["two", "three", "eleven", "so many", "too many", "an unsatisfying number of", "barely any", "an unspecified amount of", "surely a satisfactory number of"],
-    looty: ["gleaming", "valuable", "esteemed", "rare", "exalted", "scintillating", "kinda gross but still usefull", "complete garbage"],
-    loots: ["coins", "chalices", "ingots", "hides", "victory points", "gems","scrolls", "bananas", "noodles", "goblins", "CS Majors", "college credits"],
-    baddies: ["orcs", "glubs", "fishmen", "cordungles", "mountain trolls", "college professors", "dragon", "evil $adventurer", "agents of chaos"],
-    message: ["call", "txt", "post", "decree", "shoutz", "tweets", "choiche", "hearkens", "harkening", "harkenening", "harkenenening", "...wait, no! Come back", "Watermelon"],
+    accusing: ["John", "Barney", "Mickey Mouse", "Bob Ross", "The Backstreet Boys", "joe", "Pennywise", "$genericName Nye the $schoolSubject Guy", "Canada", "Mars", "$genericName and $genericName", "$genericName of planet $planets", "Geoff"],
+    defending: ["Goofy", "a tree", "Heimlich", "Gary the Ogre", "Batman", "Winnie the Pooh"],
+    genericName: ["Bill", "Steve", "Peter", "Robert", "Billy", "Larry", "David", "Tyler", "Cody", "Wesley"],
+    schoolSubject: ["Math", "Science", "English", "History", "P.E.", "Art", "Quantum Physics"],
+    time: ["morning", "afternoon", "evening"],
+    absurdTypeWord: ["outrageous", "despicable", "utterly absurd", "wicked", "totally gnarley", "horrifying"],
+    notOrIndeed: ["not", "indeed"],
+    crime: ["commit tax fraud", "jaywalk", "set the entirety of Canada on fire", "rob a hot dog stand of its mustard", "hatch millions of frog eggs in a suburban neighborhood, causing massive ecological damage to the area"],
+    proposition: ["think hard about this decision, my client's life rests in your hands.", "make this quick, I have a date in thirty minutes.", "take as long as you need. My client and I eagerly await your decision."],
+    guiltyStatus: ["guilty! \n\n  Alrighty then,", "not guilty! \n\n Well y'all are wrong!"],
+    planets: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto... but sir, Pluto isn't a plan- SHUSH! IT IS TO ME! Anyways..."],
+    courtType: ["Stinky", "Superior", "Strange", "Sus", "Superstitious", "Supremely Overrated"],
+    name: ["Williams", "Squilliam Fancyson", "Squarepants", "Lebron James"],
+    num: ["zero", "three", "eleven", "five-thousand", "negative fifteen"],
+    vagueNumbers: ["$num", "a tiny amount of", "less than two", "A MASSIVE AMOUNT OF", "an infinite amount of"],
+    timeUnits: ["seconds", "minutes", "hours", "days", "weeks", "months", "years", "decades", "centuries", "millenia"],
+    people: ["citizens", "noobs", "humans"],
+    punishment: ["listening to the barney theme song", "not eating pizza", "having a warm pillow", "having the hiccups", "being chased by an unkillable snail that will kill you the second it touches you"]
+
     
   };
   
-  const template = `$adventurer, heed my $message!
+  const template = `All rise. Department $num of the $courtType Court is now in session. Judge $name presiding. Please be seated.
   
-  I have just come from $pre$post where the $people folk are in desperate need. Their town has been overrun by $baddies. You must venture forth at once, taking my $item, and help them.
+  Good $time, $people. Calling the case of $accusing versus $defending. Are both sides ready?
   
-  It is told that the one who can rescue them will be awarded with $num $looty $loots. Surely this must tempt one such as yourself!
+  Yatta Yatta Yatta skipping a few parts
+  
+  Your honor, this is $absurdTypeWord! My client did $notOrIndeed $crime! Only a fool would think otherwise. To the jury, I ask that you $proposition.
+  
+  A few $timeUnits later...
+  
+  We the jury find the defendent... $guiltyStatus I hereby sentence you to $vagueNumbers $timeUnits of $punishment.
+  
+  Case dismissed!
   `;
   
   
@@ -41,9 +58,6 @@ const fillers = {
   
     /* global box */
     box.innerText = story;
-
-    let grammar = tracery.createGrammar(spellbook);
-    console.log(grammar.flatten('#origin#'))
   }
   
   /* global clicker */
